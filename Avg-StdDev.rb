4cedf1c466dc98b850a8a77cfc5e3257c7d396e7
Alfredo Gallegos <Jose.GallegosChavarria@xerox.com>
2015-09-20 16:37:24 -0500

    Initial Commit

---
+++ Avg-StdDev.rb
@@ -0,0 +1,31 @@
+#Programa que calcula la media y la desviacion estandar de un conjunto de n numeros reales leidos desde stdin separados por " "
+#Utilizar listas encadenadas para almacenar los datos
+
+def average(numbers)
+  return numbers.reduce(:+) / numbers.length
+end
+
+def stdDev(numbers, average)
+  return Math.sqrt( ( numbers.map{ |i| ( i - average ) ** 2 }.reduce(:+)) / ( numbers.length - 1 ) )
+end
+
+#capture from stdin and parse as floats
+numbers = gets.split(" ").map!(&:to_f)
+
+if numbers.size == 0
+  exit
+else
+  #show input
+  print "Numeros:"
+
+  numbers.each do |i|
+    print " #{i}"
+  end
+
+  #perform calculations
+  avg = average(numbers)
+  stdev = stdDev(numbers, avg)
+
+  #output calculations
+  puts "\nAverage: %.2f Standard Deviation: %.2f" % [ avg, stdev ]
+end
